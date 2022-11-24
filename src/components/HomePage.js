d64edc7b97fd8484f070e4d01e51e5c437b0acf5
Ashish-Krishna-K <ashishk.krishna@gmail.com>
2022-11-24 16:55:19 +0530

    Finalize project for submission

---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+export default function Home () {
+    return (
+        <div id="home-page">
+            <h1>Home</h1>
+            <p>You can buy wall posters of wanted posters for every strawhats here!</p>
+            <p>Click on the shop tab above to browse the catalog!</p>
+            <p>Just Kidding, this is a fake store, you can't buy anything here!</p>
+        </div>
+    )
+}
