2f373c099d30c34685421d39db0829d3a1f3b8f1
ashish-krishna-k <ashishk.krishna@gmail.com>
2023-09-18 23:25:55 +0530

    Creates basic routes.

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,27 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <h1>Hello, World!</h1>
-    }
-  ])
+      element: <h1>Home</h1>,
+      errorElement: <h1>Error</h1>
+    },
+    {
+      path: "/shop",
+      element: <h1>Shop</h1>
+    },
+    {
+      path: "/product/:productId",
+      element: <h1>Product</h1>
+    },
+    {
+      path: "/checkout",
+      element: <h1>Checkout</h1>,
+    },
+  ]);
 
-  return <RouterProvider router={router} />
-}
+  return <RouterProvider router={router} />;
+};
 
 export default Router;
