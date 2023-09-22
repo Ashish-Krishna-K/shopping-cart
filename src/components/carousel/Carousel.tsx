4617641a145e5b8b981d607ee25ea6f30a7373ac
ashish-krishna-k <ashishk.krishna@gmail.com>
2023-09-22 15:06:29 +0530

    Moves the setTimeout inside a useEffect

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CarouselPropsType } from "../../appTypes";
+import styles from './Carousel.module.css';
 
 const Carousel = ({ products }: { products: CarouselPropsType[] }) => {
   const [currentImg, setCurrentImg] = useState<CarouselPropsType>(products[0]);
-  setTimeout(() => {
+  useEffect(() => {
+    const changeImage = setTimeout(() => {
+    console.log('checking');
     const currentIndex = products.findIndex(
       (item) => item.id === currentImg.id,
     );
@@ -11,12 +14,21 @@ const Carousel = ({ products }: { products: CarouselPropsType[] }) => {
       currentIndex + 1 >= products.length ? 0 : currentIndex + 1;
     setCurrentImg(products[nextIndex]);
   }, 3000);
+  return () => {
+    clearTimeout(changeImage);
+  }
+  }, [products, currentImg])
   return (
-    <div>
-      <img src={currentImg.image} alt={currentImg.title} />
-      <div>
+    <div className={styles.carousel}>
+      <img 
+        src={currentImg.image} 
+        alt={currentImg.title} 
+        className={styles.productImg}  
+      />
+      <div className={styles.buttons}>
         {products.map((product) => (
           <button
+            className={product.id === currentImg.id ? styles.current : ""}
             key={product.id}
             onClick={() => {
               setCurrentImg(product);
